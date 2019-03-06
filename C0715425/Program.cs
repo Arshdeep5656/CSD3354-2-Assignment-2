3ccc461b69cd8e36bb3add4f8a58f78607654a26
visualstudio <visualstudio@122-PC09>
2019-03-06 11:16:06 -0500

    gr

--- C0715425/Program.cs
+++ C0715425/Program.cs
@@ -4,12 +4,160 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0715425
+
+namespace DelegateAndEvents
 {
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegeate();
+        }
+
+        
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+        }
+     {
+        public class DelegateExercises
+        {
+            public delegate void MyDelegate();
+            void Method1()
+            {
+                Console.WriteLine("Method1");
+                Console.ReadLine();
+            }
+            public void Method2()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                myDelegate(50);
+            }
         }
     }
 }
+
+
+  class Program
+{
+    static void Main(string[] args)
+    {
+        // TO DO: Call Method2 from the DelegateExercises Class
+    }
+}
+
+    public class DelegateExercises
+    {
+    public delegate void MyDelegate();
+    void Method1(int i)
+    {
+        Console.WriteLine("Method1");
+        Console.ReadLine();
+    }
+    public void Method2()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        myDelegate();
+    }
+
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate(int intValue);
+
+        public int Method1(int intMethod1)
+        {
+            return intMethod1 * 2;
+        }
+        public int Method2(int intMethod2)
+        {
+            return intMethod2 * 10;
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            int result1 = myDelegate(10);
+            System.Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            System.Console.WriteLine(result2);
+        }
+
+        public class Program
+        {
+            public static void Main()
+            {
+                DelegateExercises delegateExercises = new DelegateExercises();
+                delegateExercises.Method3();
+            }
+        }
+
+        public class DelegateExercises
+        {
+            public delegate int MyDelegate();
+
+            void Method1()
+            {
+                System.Console.WriteLine("MyDelegate");
+            }
+             public void Method3()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                myDelegate();
+            }
+
+        }
+        public class Program
+        {
+            public static void Main()
+            {
+                DelegateExercises delegateExercises = new DelegateExercises();
+                delegateExercises.Method3();
+                Console.ReadLine();
+            }
+        }
+
+        public class DelegateExercises
+        {
+            public delegate int MyDelegate(int intValue);
+
+            int Method1(int intMethod1)
+            {
+                return intMethod1 * 2;
+            }
+
+            int Method2(int intMethod1)
+            {
+                return intMethod1 * 10;
+            }
+
+            public void Method4(MyDelegate myDelegate)
+            {
+                int result = myDelegate(10);
+                Console.WriteLine(result);
+            }
+
+            public void Method3()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                Method4(myDelegate);
+                myDelegate = new MyDelegate(Method2);
+                Method4(myDelegate);
+            }
+        }
+    
+}
+
+
